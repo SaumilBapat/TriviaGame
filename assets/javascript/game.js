5c42ba30de236795e1e6c8ddaf63c5d03e3e4f1c
SaumilBapat <sbapat@salesforce.com>
2018-10-26 00:06:22 -0400

    Finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
 var questions = [
   {question: "Mos Eisley spaceport. You will never find a more wretched hive of scum and villainy.", answers: {"QUI-GON JINN": false, "HAN SOLO": false, "ANAKIN SKYWALKER": false, "OBI-WAN KENOBI": true}, image: "./assets/images/question1.jpg"},
   {question: "Name this character", answers: {"ADMIRAL MOTTI": false, "ADMIRAL PIETT": false, "COMMANDER JERJERROD": false, "GRAND MOFF TARKIN": true}, image: "./assets/images/question2.jpg"},
+  {question: "Who cut off half of Anakin Skywalker's right arm with a lightsaber?", answers: {"GENERAL GREIVOUS": false, "DARTH MAUL": false, "OBI-WAN KENOBI": false, "COUNT DOOKU": true}, image: "./assets/images/question3.jpg"},
+  {question: "I find your lack of faith disturbing.", answers: {"OBI-WAN KENOBI": false, "DARTH VADER": true, "THE EMPEROR": false, "QUI-GON JINN": false}, image: "./assets/images/question4.jpg"},
 ];
 var wrongAnswers = 0;
 var correctAnswers = 0;
-var unanswer = 0;
+var unanswered = 0;
 
-var timer = 5;
+var timer = 10;
 var timerInterval;
 var currentQuestion = 0;
 
@@ -14,29 +16,42 @@ $(document).ready(function() {
   startGame();
 });
 
+
+var timer = {
+  count: 10,
+  timerInterval,
+  resetTimer: function() {
+    $("#Timer").empty();
+    clearInterval(timer.timerInterval);
+    this.count = 10;
+  },
+  renderTimer: function() {
+    $("#Timer").empty();
+    var time = $("<div>").html("<div class='userMessage timer'><h1>" + this.count + "</h1></div>");
+    $("#Timer").append(time);
+  },
+  countDown: function() {
+    timer.count = timer.count - 1;
+    if(timer.count < 0) {
+      unanswered++;
+      renderNext();
+    } else {
+      timer.renderTimer();
+    }
+  },
+  startTimer: function() {
+    timer.timerInterval = setInterval(timer.countDown, 1000);
+  },
+};
+
 function startGame() {
   renderQuestions();
 }
 
-function renderTimer() {
-  if(timer < 0) {
-    clearInterval(timerInterval);
-    $("#Timer").empty();
-    wrongAnswers++;
-    currentQuestion++;
-    timer = 5;
-    renderNext();
-  } else {
-    $("#Timer").empty();
-    var time = $("<div>").html("<div class='userMessage timer'><h1>" + timer + "</h1></div>");
-    $("#Timer").append(time);
-    timer--;
-  }
-}
 
 function renderQuestions() {
-  clearInterval(timerInterval);
-  timerInterval = setInterval(renderTimer, 1000);
+  timer.resetTimer();
+  timer.startTimer();
   $("#Questions").empty();
   var question = $("<div>").html("<div class='userMessage'>" + questions[currentQuestion].question + "</div>");
   $("#Questions").append(question);
@@ -59,14 +74,20 @@ function checkAnswer() {
     correctAnswers++;
     alert("Correct!");
   } else {
+    var rightAnswer;
+    for(var answer in questions[currentQuestion].answers) {
+      if(questions[currentQuestion].answers[answer]) {
+        rightAnswer = answer;
+      }
+    }
     wrongAnswers++;
-    alert("Wrong!");
+    alert("Wrong! The correct answer is: " + rightAnswer);
   }
-  currentQuestion++;
   renderNext();
 }
 
 function renderNext() {
+  currentQuestion++;
   if(currentQuestion >= questions.length) {
     displayResults();
   } else {
@@ -75,11 +96,12 @@ function renderNext() {
 }
 
 function displayResults() {
-  clearInterval(timerInterval);
+  timer.resetTimer();
   $("#Questions").empty();
-  $("#Timer").empty();
   var correctAnswersDiv = $("<div>").html("<div class='userMessage'>Correct Answers: " + correctAnswers + "</div>");
   $("#Questions").append(correctAnswersDiv);
   var wrongAnswersDiv = $("<div>").html("<div class='userMessage'>Wrong Answers: " + wrongAnswers + "</div>");
   $("#Questions").append(wrongAnswersDiv);
+  var unansweredDiv = $("<div>").html("<div class='userMessage'>Unanswered Questions: " + unanswered + "</div>");
+  $("#Questions").append(unansweredDiv);
 }
